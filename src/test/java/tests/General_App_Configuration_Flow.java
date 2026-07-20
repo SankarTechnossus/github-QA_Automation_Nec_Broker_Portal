00f17479cd901fd32c60257258f0845204f904f9
SankarVenkatesan <shankarvenkatesan59@gmail.com>
2026-07-20 20:00:35 +0530

    Init Commit : Date : 20July2026 Time:7:37

--- src/test/java/tests/General_App_Configuration_Flow.java
+++ src/test/java/tests/General_App_Configuration_Flow.java
@@ -1,18 +1,25 @@
 package tests;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
 import base.BasePage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import listeners.ExtentReportListener;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-import pages.*;
+import pages.DashboardPage;
+import pages.GeneralPage;
+import pages.LoginPage;
 import utils.DriverManager;
 
-public class General_App_Configuration_Flow  {
+import java.time.Duration;
+
+@Listeners(ExtentReportListener.class)
+public class General_App_Configuration_Flow {
+
     WebDriver driver;
     WebDriverWait wait;
     BasePage basePage;
@@ -26,17 +33,23 @@ public class General_App_Configuration_Flow  {
         WebDriverManager.chromedriver().setup();
 
         driver = new ChromeDriver();
-
         DriverManager.setDriver(driver);
 
         driver.manage().window().maximize();
 
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
         basePage = new BasePage(driver);
         loginPage = new LoginPage(driver);
         dashboardPage = new DashboardPage(driver);
         generalPage = new GeneralPage(driver);
+
+        // Open application
+        driver.get("https://mobileidwebqanfw-phase2.azurewebsites.net");
+
+        // Login
+        loginPage.LoginIntoApplication("Sankar", "Sankar");
+
     }
 
     @Test(description = "Update General Configuration")
@@ -52,7 +65,10 @@ public class General_App_Configuration_Flow  {
 
         ExtentReportListener.getExtentTest().info("Updating Similarity Score Label");
         ExtentReportListener.getExtentTest().pass("General Configuration updated successfully.");
-
     }
 
+    @AfterMethod
+    public void tearDown() {
+        DriverManager.quitDriver();
+    }
 }
